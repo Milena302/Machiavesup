b652f2d3a66138295dcd619c02c054e0f0a6aa7c
Etienne Lozes <elozes@unice.fr>
2023-03-31 16:32:58 +0200

    gros chamboulement du prof

--- src/GaleShapley.java
+++ src/GaleShapley.java
@@ -6,110 +6,13 @@ public class GaleShapley {
         int size = 8;   //Taille de chaque echantillon
         int seed = 25;  //Pour l'aleatoire
 
-        ArrayList<Disposants> disposants = new ArrayList<>();
-        ArrayList<Proposants> proposants = new ArrayList<>();
+        Instance i = new Instance(size, seed);
 
-        //Remplissage des disposants
-        for (int i = 0; i < size; i++) {
-            Disposants newDisposant = new Disposants(i, size, seed);
-            newDisposant.genererListeSouhait();
-            seed++;
-            disposants.add(newDisposant);
-        }
+        Mariages couples = i.runGS();
 
-        //Remplissage des proposants
-        for (int i = 0; i < size; i++) {
-            Proposants newProposants = new Proposants(i, size, seed);
-            newProposants.getPref();
-            seed++;
-            proposants.add(newProposants);
-        }
+        couples.affiche();
 
-        //Tri par ordre croissant des pos dans chaque liste de préférences
-        for (Proposants p : proposants) {
-            p.trigetCouples();
-        }
-        for (Disposants d : disposants) {
-            d.getCouples();
-        }
-
-        //Affichage des disposants
-        for (Disposants d : disposants) {
-            System.out.println("Disposant " + d.getId() + ": " + Arrays.toString(d.getCouples()));
-        }
-
-        //Affichage des proposants
-        System.out.println("Contenu des proposants : ");
-        for (Proposants p : proposants) {
-            System.out.println("Proposant " + p.getId() + ": " + Arrays.toString(p.trigetCouples()));
-        }
-
-        Map<Integer, Integer> couples = new HashMap<>();
-        //Boucle principale de l'algorithme
-        while (couples.size() < size) {
-            //Recherche d'un disposant non-apparié
-            Disposants disposant = null;
-            for (Disposants d : disposants) {
-                if (!couples.containsKey(d.getId())) {
-                    disposant = d;
-                    break;
-                }
-            }
-            if (disposant == null) {
-                break; //Tous les disposants sont appariés
-            }
-
-            //Sélection du premier proposant dans la liste de souhaits du disposant
-            Proposants proposant = proposants.get(disposant.getSouhait(0).getId());
-            couples.put(disposant.getId(), proposant.getId());
-            proposant.setCouple(disposant.getId());
-            // Boucle de recherche de meilleur proposant
-            for (int i = 1; i < size; i++) {
-                int proposantId = disposant.getSouhait(i).getId();
-                Proposants nouveauProposant = proposants.get(proposantId);
-
-                if (!couples.containsValue(nouveauProposant.getId())) {
-                    // Le proposant est libre, vérifier qu'il n'est pas marié avec un autre disposant
-                    boolean alreadyMarried = false;
-                    for (Map.Entry<Integer, Integer> couple : couples.entrySet()) {
-                        if (couple.getValue() == nouveauProposant.getId()) {
-                            alreadyMarried = true;
-                            break;
-                        }
-                    }
-                    if (!alreadyMarried) {
-                        // Le proposant est apparié avec le disposant
-                        couples.put(disposant.getId(), nouveauProposant.getId());
-                        nouveauProposant.setCouple(disposant.getId());
-                        proposant = nouveauProposant;
-                    } else if (disposant.proposantPrefere(proposant.getPosition(proposantId), nouveauProposant.getPosition(nouveauProposant.getId()))) {
-                        // Le disposant préfère le nouveau proposant
-                        couples.remove(nouveauProposant.getCouple());
-                        Proposants ancienProposant = proposants.get(nouveauProposant.getCouple());
-                        ancienProposant.removeCouple(); // Supprime la référence à l'ancien couple
-                        couples.put(disposant.getId(), nouveauProposant.getId());
-                        nouveauProposant.setCouple(disposant.getId());
-                        proposant = nouveauProposant;
-                    }
-                }
-            }
-        }
-
-        //Affichage des couples formés
-        System.out.println("Couples formés : ");
-        for (Map.Entry<Integer, Integer> couple : couples.entrySet()) {
-            System.out.println("Disposant " + couple.getKey() + " \t-\tProposant " + couple.getValue());
-        }
-
-        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(couples.entrySet());
-        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
-            @Override
-            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
-                return o1.getValue().compareTo(o2.getValue());
-            }
-        });
-        System.out.println("Sorted map by value: " + list);
-    }
+        couples.afficheParProposant();
 }
 
 /**
